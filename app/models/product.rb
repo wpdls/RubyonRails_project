cff5550f538d1ff29304d8135c5144a1fbaba40a
Jayne Phillips <jaynephillips61@gmail.com>
2020-01-19 17:52:39 +0100

    Added validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,14 @@
 class Product < ApplicationRecord
+    validates :title, :description, :image_url, presence: true
+    validates :price, numericality: { greater_than_or_equal_to: 0.01 }
+    validates :title, uniqueness: true
+    validates :image_url, allow_blank: true, format: {
+        with: %r{\.(gif|jpg|png)\Z}i,
+        message: 'must be a URL for GIF, JPG or PNG image.'
+        }
 end
+
+# The title, description, and image URL fields aren’t empty.
+# The price is a valid number not less than $0.01.
+# The title is unique among all products.
+# The image URL looks reasonable
