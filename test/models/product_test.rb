55dd55912e54c6392812f9e56014b8f784595630
Jayne Phillips <jaynephillips61@gmail.com>
2020-01-19 18:20:13 +0100

    Test fixtures

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,12 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "product is not valid without a unique title" do
+    product = Product.new(title: products(:ruby).title,
+    description: "yyy",
+    price: 1,
+    image_url: "fred.gif")
+    assert product.invalid?
+    assert_equal ["has already been taken"], product.errors[:title]
+    end
 end
