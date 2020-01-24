a04cef4367686862e3a9030f7b4457d63907fac3
Jayne Phillips <jaynephillips61@gmail.com>
2020-01-24 11:53:28 +0100

    Adding multiples of the same item to the cart

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,4 +1,14 @@
 class Cart < ApplicationRecord
     has_many :line_items, dependent: :destroy #dependent part indicated that the existence of line items is dependent on the existnece of the cart
     # if we destroy a cart, also destory any line items that are associated with that cart
+
+    def add_product(product)
+        current_item = line_items.find_by(product_id: product.id)
+            if current_item
+             current_item.quantity += 1
+            else
+             current_item = line_items.build(product_id: product.id)
+            end
+        current_item
+        end
 end
