111f190756713f93a41c82654b37ee4d5eef6be3
Jayne Phillips <jaynephillips61@gmail.com>
2020-01-26 20:41:11 +0100

    Avoid displaying the cart at all unless the value is set

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -30,7 +30,7 @@ class LineItemsController < ApplicationController
       @line_item = @cart.add_product(product)
       respond_to do |format|
         if @line_item.save
-        format.html { redirect_to @line_item.cart}
+        format.html { redirect_to store_index_url }
         format.json { render :show,
           status: :created, location: @line_item }
         else
