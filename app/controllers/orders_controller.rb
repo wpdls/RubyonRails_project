1bf6a0fe712522adfe56508ac085df07c283e357
Jayne Phillips <jaynephillips61@gmail.com>
2020-01-27 22:41:18 +0100

    Using React components for different payment types

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -69,6 +69,18 @@ class OrdersController < ApplicationController
     end
   end
 
+  def pay_type_params
+    if order_params[:pay_type] == "Credit Card"
+      params.require(:order).permit(:credit_card_number, :expiration_date)
+    elsif order_params[:pay_type] == "Check"
+      params.require(:order).permit(:routing_number, :account_number)
+    elsif order_params[:pay_type] == "Purchase Order"
+      params.require(:order).permit(:po_number)
+    else
+      {}
+    end
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_order
