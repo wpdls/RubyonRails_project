109699c7eab9b758e4be33345ccad5868d85552b
Jayne Phillips <jaynephillips61@gmail.com>
2020-01-28 15:08:06 +0100

    Use a transaction to prevent deletion of last user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,13 @@
 class User < ApplicationRecord
   validates :name, presence: true, uniqueness: true #uniquneness is when no two users are same
   has_secure_password # ask to enter pswd twice and validates it #because we put password:digest when generated scaffold
+  after_destroy :ensure_an_admin_remains
+  class Error < StandardError
+  end
+  private
+    def ensure_an_admin_remains
+    if User.count.zero?
+    raise Error.new "Can't delete last user"
+    end
+  end
 end
